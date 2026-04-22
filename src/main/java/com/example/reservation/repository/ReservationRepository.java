0a9a17a81cf4f331220d6f3298c6578f6ed2bff1
parimalprince9-max <parimalprince9@gmail.com>
2026-04-22 07:41:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/reservation/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.example.reservation.repository;
+
+import com.example.reservation.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
